13ce1387b31d32e355a6b5f15080672fe037ae9c
Muhamad Farhan Rajab <farhanrajab15@gmail.com>
2022-11-08 17:42:37 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/commit/';
+// $config['base_url'] = 'http://localhost/commit/';
+$config['base_url'] = 'https://ukm-commit-uika.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
